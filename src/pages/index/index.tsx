b70dfb45a22777a048efcb8f7dcd6cda0377026c
cailin <cailinunix@gmail.com>
2020-05-18 00:06:31 +0800

    justify styles of index

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -18,15 +18,17 @@ const Index = () => {
         <ul>
           {postList.map((item) => (
             <li className="post-list-item" key={item.postId}>
-              <span className="reply-count">{item.viewCount}</span>
-              <div className="left">
-                <a className="user-avator" href={item.author.uid}>
-                  <img src={item.author.avator} />
-                </a>
+              <a className="user-avator" href={item.author.uid}>
+                <img src={item.author.avator} />
+              </a>
+              <div className="center">
                 <span className="normal-tag">分享</span>
-                <a className="post-title" href={item.postId}>
-                  {item.postTitle}
-                </a>
+                <div className="post-title-box">
+                  <a className="post-title" href={item.postId}>
+                    {item.postTitle}
+                  </a>
+                  <span className="reply-count">{item.viewCount}</span>
+                </div>
               </div>
               <a className="last-active-time">1小时之前</a>
             </li>
